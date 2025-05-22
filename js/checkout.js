6db7d69e57fcc6652496f02d5da79659b97edb46
Juan Delgado <soy.juan.delgado@gmail.com>
2025-05-22 18:44:32 +0200

    All check functions to arrow functions

--- js/checkout.js
+++ js/checkout.js
@@ -63,131 +63,33 @@ function validate() {
 		validForm = validInput && validForm;
 	});
 
-	
-
-	// Get the input fields
-/* 	const fName = document.getElementById("fName");
-	const fLastN = document.getElementById("fLastN");
-	const fEmail = document.getElementById("fEmail");
-	const fAddress = document.getElementById("fAddress");
-	const fPassword = document.getElementById("fPassword");
-	const fPhone = document.getElementById("fPhone");
-
-	// Get the error elements
-	const errorName = document.getElementById("errorName");
-	const errorLastN = document.getElementById("errorLastN");
-	const errorEmail = document.getElementById("errorEmail");  
-	const errorPhone = document.getElementById("errorPhone");   */
-	
-	// Validate fields entered by the user: name, phone, password, and email
-/*  	if(!validName(fName.value)) {
-		validForm = false;
-		fName.classList.add("is-invalid");
-	}else{
-		fName.classList.remove("is-invalid");
-	} */
-
-	
-/* 	if(!validName(fLastN.value)){
-		validForm = false;
-		fLastN.classList.add("is-invalid");
-	}
-	else{
-		fLastN.classList.remove("is-invalid");
-	} */
-
-/* 	if(!validLength(fAddress.value, 3)){
-		validForm = false;
-		fAddress.classList.add("is-invalid");
-	}else{
-		fAddress.classList.remove("is-invalid");
-	} */
-
-/* 
-	if(!validPhone(fPhone.value)){
-		validForm = false;
-		fPhone.classList.add("is-invalid");
-	}else{
-		fPhone.classList.remove("is-invalid");
-	} */
-
-	//var form = document.getElementById("form");
-	//form.classList.add("was-validated")
-	//event.preventDefault()
-	
 	return validForm;
 }
 
-function validName(value){
-	let valid = true;
-	if (typeof value !== 'string') return false;
+const validName = (value) => 
+	(typeof value === 'string') &&
+	validLength(value, 3) &&
+	!containsNumber(value);
 
-	//console.log(`value = '${value}'`);
-	if(!validLength(value, 3)){
-		//console.log(`length = '${value.length}'`);
-		valid = false;
-	}else if(containsNumber(value)){
-		//console.log(`contains number = '${value}'`);
-		valid = false;
-	}
-	return valid;
-}
+const validPhone = (value) => 
+	(typeof value === 'string') &&
+	validLength(value, 9, 9) &&
+	onlyNumbers(value);
 
-function validPhone(value){
-	let valid = true;
-	if (typeof value !== 'string') return false;
+const validEmail = (value) =>
+	(typeof value === 'string') &&
+	validLength(value, 3) &&
+	checkEmail(value);
 
-	//console.log(`validPhone value = '${value}'`);
-	
-	if (!validLength(value, 9, 9)){
-		valid = false;
-	}else if (!onlyNumbers(value)){
-		valid = false;
-	}
-	return valid;
-}
+const validPassword = (value) => 
+	(typeof value === 'string') &&
+	validLength(value, 3) &&
+	checkPassword(value);
 
-function validEmail(value){
-	if (typeof value !== 'string') return false;
-
-	if(!validLength(value, 3))
-		return false;
-	console.log(`Email len ok. ${value}`);
-	if (!checkEmail(value))
-		return false;	
-	return true;
-}
-
-function validPassword(value){
-	if (typeof value !== 'string') return false;
-	if(!validLength(value, 3))
-		return false;
-	if(!checkPassword(value))
-		return false;
-	return true;
-}
-
-function validLength(value, min, max = 1000){
-	let valid = true;
-/* 	if (typeof value !== 'string') return false;
-	
-	const length = value.trim().length;
-
-	console.log(`validLength: '${value}', length=${length}, min=${min}, max=${max}`);
-	return length >= min && length <= max; */
-
-	if (typeof value !== 'string') return false;
-
-	//console.log(`validLength: ${value}, ${min}, ${max}`);
-	if(!value){
-		valid = false;
-	} else if(value.length < min){
-		valid = false;
-	}else if(value.length > max){
-		valid = false;
-	}
-	return valid;
-}
+const validLength = (value, min, max = 1000) =>
+	(typeof value === 'string') &&
+	value.length >= min &&
+	value.length <= max;
 
 const containsNumber = (value) => /\d/.test(value);
 
