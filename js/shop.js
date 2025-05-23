82724416e4b88d39260a35ffd3f9a464906451fa
Juan Delgado <soy.juan.delgado@gmail.com>
2025-05-23 12:16:21 +0200

    fix: buy - remove console #10

--- js/shop.js
+++ js/shop.js
@@ -77,25 +77,18 @@ var total = 0;
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
-    console.log("Buy Item")
     let product = products.find( p => p.id == id)
-    console.log(product)
     // 2. Add found product to the cart array
     let product2 = cart.find( p => p.id == id)
     if(!product2){
         product.quantity = 1;
-        console.log("Product quantity = 1")
         console.log(product)
         cart.push(product)
     }else{
         product.quantity++;
-        console.log("Product quantity++")
         console.log(product)
     }
     console.log(`Cart: added ${product.name}, quantity ${product.quantity}`)
-    console.log(cart)
-    console.log("Products")
-    console.log(products)
     modifyCountProduct(1)
 }
 
